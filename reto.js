190d94e369b8e3aad467c5c6bdccfacc5ecee535
jess <jesicatorti@gmail.com>
2022-03-07 21:51:22 -0300

    improve

--- reto.js
+++ reto.js
@@ -10,47 +10,42 @@ const elemento5 = document.getElementById("elemento5");
 const b = document.getElementById("button");
 b.addEventListener("click", calcularMediana);
 
-function calcularMediana() {
+function calcularMediana(){
     list[0]= parseInt(elemento1.value);
     list[1]= parseInt(elemento2.value);
     list[2]= parseInt(elemento3.value);
     list[3]= parseInt(elemento4.value);
     list[4]= parseInt(elemento5.value);
-
-      // const lista= list.length / 2;
-    alert("the median is " + list);
+    const lista= list.length / 2;
+ 
+    function esPar(lista)
+    {
+        if (lista %2 === 0 ) 
+        {
+        return true;
+        }   
+        else 
+        {
+        return false;
+        } 
+    function calcularMediaAritmetica(list)
+        {
+        const sumaLista = list.reduce (function (valorAcumulado = 0, nuevoElemento) {
+        return valorAcumulado + nuevoElemento;});
+        const medianaLista = sumaLista / list.length;          
+        return medianaLista;
+        }
+    }
+    if (esPar(list.length))
+    {
+    const elemento1 = lista[medianaLista - 1];
+    const elemento2 = lista[medianaLista];
+    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2, ]);
+    mediana = promedioElemento1y2;
+    }   
+    else 
+    {    
+    mediana = lista[parseInt(mitadLista)];    
+    }
+    alert("the median is " + mediana);
 }
-
-
-// }
-// function esPar(numerito)
-// {
-//     numerito = lista.length;
-//     if (numerito %2 === 0 ) 
-//     {
-//     return true;
-//     }   
-//     else 
-//     {
-//     return false;
-//     } 
-// }
-// function calcularMediaAritmetica(list){
-// const sumaLista = list.reduce (function (valorAcumulado = 0, nuevoElemento) {
-// return valorAcumulado + nuevoElemento;});
-// const medianaLista = sumaLista / list.length;          
-// return medianaLista;
-// }
-// if (esPar(lista1.length))
-// {
-// const elemento1 = lista[mitadLista - 1];
-// const elemento2 = lista[mitadLista];
-// const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2, ]);
-// mediana = promedioElemento1y2;
-// }
-// else {    
-// mediana = lista[parseInt(mitadLista)];    
-// }
-// alert("the median is " + lista);
-// alert("the median is " + median);
-// }
